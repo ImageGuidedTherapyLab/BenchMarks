033f149edb51ab781e85ef45f8499eed74d9ae13
David Fuentes <fuentesdt@gmail.com>
2016-04-20 09:07:45 -0500

    WIP: linpack scaling

--- linpack_driver.m
+++ linpack_driver.m
@@ -1,23 +1,29 @@
+clear all
+close all
 
 
 SizeArray = [5000:5000:100000];
+SizeArray = [1000:1000:10000];
 
-FlopPerformance = zeros(length(SizeArray ));
-TimePerformance = zeros(length(SizeArray ));
+FlopPerformance = zeros(1,length(SizeArray ));
+TimePerformance = zeros(1,length(SizeArray ));
 
 for iii = 1:length(SizeArray )
    timings = linpack_bench_backslash(SizeArray(iii));
-   FlopPerformance = timings(4);
-   TimePerformance = timings(3);
+   FlopPerformance(iii) = timings(4);
+   TimePerformance(iii) = timings(3);
 end
 
 
 handle1=figure(1);
-plot(SizeArray,FlopPerformance )
-hold
+subplot(2,1,1),plot(SizeArray,TimePerformance );
+ylabel('time [s]'  ) % axis
+title('Linpack')
 set(gca,'FontSize',16)
+subplot(2,1,2),plot(SizeArray,FlopPerformance );
+ylabel('MFLOPs') % axis
 xlabel('N')
-ylabel('MFLOPs')
-legend('linpack', 'Location','SouthEast')
+set(gca,'FontSize',16)
+
 saveas(handle1,'linpack','png')
 
